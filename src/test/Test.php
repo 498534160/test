e72a75ee67582bd993468d4d30fe2bd07fc8dc52
雨沫 <180705yumo@gmail.com>
2020-07-21 15:33:57 +0800

    测试代码

---
+++ src/test/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace test;
+
+
+class Test
+{
+    public static function index()
+    {
+        return 'test success';
+    }
+
+    public static function home()
+    {
+        return 'test home';
+    }
+
+}
